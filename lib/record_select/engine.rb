7a70d1d200d5325578a7fdf152d6c21cb09aac8c
Sergio Cambra <sergio@programatica.es>
2016-03-28 09:40:00 +0200

    add images used in views to assets precompile, for rails 4.2

--- lib/record_select/engine.rb
+++ lib/record_select/engine.rb
@@ -1,4 +1,5 @@
 module RecordSelect
   class Engine < Rails::Engine
+    config.assets.precompile << 'record_select/next.gif' << 'record_select/previous.gif' if Rails::VERSION::MAJOR >= 4
   end
 end
