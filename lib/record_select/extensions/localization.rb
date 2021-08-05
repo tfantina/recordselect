5cc14359fee1ddfdacfa20c702ce81b6fe07133e
Travis Fantina <travis@objectiveinc.com>
2021-08-05 10:35:07 -0400

    Updates I18n to pass hash as keyword argument
    Hashes have to be passed as keyword arguments in Rails 2.7 +

--- lib/record_select/extensions/localization.rb
+++ lib/record_select/extensions/localization.rb
@@ -4,10 +4,10 @@
 class Object
   def rs_(key, options = {})
     unless key.blank?
-      text = I18n.translate "#{key}", {:scope => [:record_select], :default => key.is_a?(String) ? key : key.to_s.titleize}.merge(options)
+      text = I18n.translate "#{key}", **{:scope => [:record_select], :default => key.is_a?(String) ? key : key.to_s.titleize}.merge(options)
       # text = nil if text.include?('translation missing:')
     end
-    text ||= key 
+    text ||= key
     text
   end
 end
